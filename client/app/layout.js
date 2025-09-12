bef1d73e8612bd0c4fdf6e6ccc52a6cebbd4e9f2
Emre Atasavun <120120633+Fridgemann@users.noreply.github.com>
2025-09-12 17:28:08 +0300

    Refactor app structure and add not-found page
    Moved main pages and layout into a new (main) directory to better organize the app structure. Added a not-found.js page for 404 handling. Updated the root layout to remove header and footer, moving them to the new (main)/layout.js.

--- client/app/layout.js
+++ client/app/layout.js
@@ -1,17 +1,4 @@
-// import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
-
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
-
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
 
 export const metadata = {
   title: "Interloper",
@@ -21,11 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        <Header />
-        {children}
-        <Footer />
-      </body>
+      <body>{children}</body>
     </html>
   );
 }

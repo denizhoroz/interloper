ccabe01dbc789eee47721fe1bce165d2852c595c
denizhoroz <denizhoroz.ofcl@gmail.com>
2025-09-17 22:41:47 +0300

    updated python service

--- server/py_service.py
+++ server/py_service.py
@@ -1,27 +1,35 @@
 # python_service.py
 from flask import Flask, request, jsonify
-# from interloper import initialize_model, Session, Evaluator
+from interloper import initialize_model, Session, Evaluator
 app = Flask(__name__)
 
 # Initialize values
-# session = None
+session = None
 session_history = None
 
 # When the service initializes
 @app.route('/', methods=['GET'])
 def home():
-    # llm = initialize_model(model_path="../models/llama-3-8b-instruct_Q4_K_M.gguf")
+    print('initializing')
 
-    return "Model is initialized!"
+    global llm
+    llm = initialize_model(model_path="../models/llama-3-8b-instruct_Q4_K_M.gguf")
+
+    print('initialized')
+    return "200" # when the model initializes
 
 # When the session is clicked
 @app.route('/session/<id>', methods=['GET'])
 def get_session(id):
-    # global session
-    # session = Session(model=llm, session_n=int(id))
+    print('session clicked')
 
-    # _, output, _ = session.generate_message() # This will take 1 minutes to execute, add timers to forbid user from texting
-    return jsonify({"message": "initialization message"}) # runs on init
+    global session
+    session = Session(model=llm, session_n=int(id))
+
+    _, output, _ = session.generate_message() # This will take 1 minutes to execute, add timers to forbid user from texting
+
+    print('session initialized')
+    return jsonify({"message": str(output)}) # runs on session init
 
 # When a message is sent
 @app.route('/process', methods=['POST'])
@@ -29,9 +37,9 @@ def process():
     data = request.json
     human_message = data['message']
 
-    # status, output, session_history = session.generate_message(input=str(human_message))
+    status, output, session_history = session.generate_message(input=str(human_message))
 
-    return jsonify({'message': "message msg", 'status': "status msg"}) # status: <END OF CONVERSATION>, <CONTINUE>
+    return jsonify({'message': str(output), 'status': str(status)}) # status: <END OF CONVERSATION>, <CONTINUE>
     # message will be bot's reply, status will indicate conversation end or continuation
 
     # print(f"Received data: {data}")
@@ -42,4 +50,4 @@ def process():
     # return jsonify(result)
 
 if __name__ == '__main__':
-    app.run(port=6000)
+    app.run(port=8000)
